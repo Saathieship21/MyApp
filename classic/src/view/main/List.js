56827c643f071674cfb69562f4b6f703b1bbfb70
HemanthKumarPasupuleti <hemanth211000@gmail.com>
2024-04-22 14:58:18 +0530

    UPCHG

--- classic/src/view/main/List.js
+++ classic/src/view/main/List.js
@@ -176,7 +176,7 @@ Ext.define('MyApp.view.main.List', {
 //                 var nextPageView = Ext.getCmp('MyApp.view.main.Login');
 // nextPageView.show();
             // },
-           //update repos
+          //CHG
             style: {
     
                 background: 'silver',
